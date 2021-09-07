3ee8e1360f8aeee65cdbb1633b5737e988a8400d
Gabriel Martins <gamartins77@gmail.com>
2021-09-07 17:45:54 -0300

    Created the test for the calculated of casbhack

---
+++ wallet/tests/test_utils.py
@@ -0,0 +1,21 @@
+from wallet.utils import calculate_cashback
+
+
+def test_calculate_cashback():
+
+    cashback = calculate_cashback(
+        [
+            {
+                "type": "A",
+                "value": 10.00,
+                "qty": 1,
+            },
+            {
+                "type": "B",
+                "value": 10.00,
+                "qty": 9,
+            },
+        ]
+    )
+
+    assert cashback == 9.5
