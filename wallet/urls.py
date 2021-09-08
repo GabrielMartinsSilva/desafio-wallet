0fbc5e9e5755b44effd84539cdfbb4cb79d4f3fd
Gabriel Martins <gamartins77@gmail.com>
2021-09-07 21:20:50 -0300

    Restructuring repository

--- wallet/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [path("cashback", views.cashback, name="cashback")]
--- wallet/utils.py
+++
@@ -1,8 +0,0 @@
-from wallet.schemas import product_type
-
-
-def calculate_cashback(products):
-    cashback = 0
-    for item in products:
-        cashback += (item["value"] * item["qty"]) * (product_type[item["type"]] / 100)
-    return cashback
