6e4e63debcbd0e044cdc58431c849c97e97a4b41
Gabriel Martins <gamartins77@gmail.com>
2021-09-07 22:03:54 -0300

    Fixed date view variable that received a fixed value

--- wallet/views.py
+++ wallet/views.py
@@ -21,10 +21,12 @@ def cashback(request):
             model.save()
         except ValidationError as err:
             return JsonResponse(err.messages, status=422)
-
+    
+    customer_document = data["customer"]["document"]
+    
     cashback_value = calculate_cashback(data["products"])
     url = "https://5efb30ac80d8170016f7613d.mockapi.io/api/mock/Cashback"
-    data = {"document": "40221945881", "cashback": cashback_value}
+    data = {"document": customer_document, "cashback": cashback_value}
     response = requests.post(url, data)
 
     return JsonResponse(response.json(), status=200)
